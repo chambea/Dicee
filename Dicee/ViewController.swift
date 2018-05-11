d91267375cf23a8b124605f6da9e38326f21fc49
Randy De Silva <randy@Randys-MacBook-Pro.local>
2018-05-10 20:56:47 -0700

    added functions using DRY principles

--- Dicee/ViewController.swift
+++ Dicee/ViewController.swift
@@ -12,13 +12,16 @@ class ViewController: UIViewController {
     
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
+    //Array that holds all the dice images
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
     
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        updatedDiceImages()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,11 +31,18 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         
-        randomDiceIndex1 = Int(arc4random_uniform(6))
-        randomDiceIndex2 = Int(arc4random_uniform(6))
-    
-        print(randomDiceIndex1)
+        updatedDiceImages()
     }
     
+    func updatedDiceImages(){
+        //arc4random_uniform()function creates random number.It is already built in UIKit. We create a variable the spits out 2 random numbers
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+
+        //selects random dice face for image views
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
+    }
+        
 }
 
